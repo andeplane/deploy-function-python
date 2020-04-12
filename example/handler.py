91d7e3823c4d9a23e6c6de861300eab56d36587a
Anders Hafreager <anders.hafreager@cognite.com>
2020-04-12 20:22:48 +0200

    Stupid me

--- example/handler.py
+++ example/handler.py
@@ -2,5 +2,5 @@ def handle(data, client, secrets):
     print("Hello anders")   
     print("Data: ", data)
     return {
-        2.0 * data["value"]
+        "result": 2.0 * data["value"]
     }
--- example2/handler.py
+++ example2/handler.py
@@ -2,5 +2,5 @@ def handle(data, client, secrets):
     print("Hello again anders")
     print("Data: ", data)
     return {
-        4.0 * data["value"]
+        "result": 4.0 * data["value"]
     }
